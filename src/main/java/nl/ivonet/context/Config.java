6df4fda63a4d98c3fdc860e51df06903cf8b87dd
Ivo Woltring <ivonet@gmail.com>
2017-08-09 21:55:45 +0200

    Doc

--- src/main/java/nl/ivonet/context/Config.java
+++ src/main/java/nl/ivonet/context/Config.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
+ * Configs have a place here.
+ *
  * @author Ivo Woltring
  */
 @Component
