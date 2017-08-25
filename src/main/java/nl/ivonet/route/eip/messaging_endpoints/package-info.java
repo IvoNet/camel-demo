43d938c704a9eb11ad0bac476d48dbf90acf6f3d
Ivo Woltring <ivonet@gmail.com>
2017-08-25 13:33:29 +0200

    Package info

---
+++ src/main/java/nl/ivonet/route/eip/messaging_endpoints/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package handles the <a href="http://www.enterpriseintegrationpatterns.com/patterns/messaging/MessagingEndpointsIntro.html">Message Endpoints</a> EIPs.
+ *
+ * @author Ivo Woltring
+ */
+package nl.ivonet.route.eip.messaging_endpoints;
---
+++ src/main/java/nl/ivonet/route/eip/messaging_endpoints/service_activator/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package handles the <a href="http://www.enterpriseintegrationpatterns.com/patterns/messaging/MessagingAdapter.html">Service Actuator</a> EIP.
+ *
+ * @author Ivo Woltring
+ */
+package nl.ivonet.route.eip.messaging_endpoints.service_activator;
